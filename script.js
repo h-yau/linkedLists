0629bd7a3836836884978b287398dd7e34714f44
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2024-04-30 15:45:33 -0700

    Build simple factories for linked list and node

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hi');
+const LinkedList = (headNode) => {
+  return { headNode };
+};
+
+const Node = (value, next) => {
+  if (!value) value = null;
+  if (!next) value = null;
+  return { value, next };
+};
