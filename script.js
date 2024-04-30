9429b3e5b8a7998eb4fb31ac24e58914d0b9cc11
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2024-04-30 16:46:52 -0700

    Prepare basic linked list structure

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-const LinkedList = (headNode) => {
-  return { headNode };
+const Node = (value = null, next = null) => {
+  return { value, next };
 };
 
-const Node = (value, next) => {
-  if (!value) value = null;
-  if (!next) value = null;
-  return { value, next };
+const LinkedList = () => {
+  let headNode = null;
+
+  const getHeadNode = () => {
+    return headNode;
+  };
+
+  const append = (value) => {
+    const newNode = Node(value);
+
+    if (!headNode) {
+      headNode = newNode;
+    } else {
+      let temp = headNode;
+      while (temp.next != null) {
+        temp = temp.next;
+      }
+      temp.next = newNode;
+    }
+
+    // just to confirm the new node that is created
+    return newNode;
+  };
+
+  const prepend = (value) => {
+    const newNode = Node(value, headNode);
+    headNode = newNode;
+
+    // just to confirm the new node that is created
+    return headNode;
+  };
+
+  return { getHeadNode, append, prepend };
 };
